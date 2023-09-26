329878727398ae2d60dd3ba15dfe75b38ba483fa
Utku Unsal <utkunsal02@gmail.com>
2023-09-26 15:41:09 +0300

    init kafka consumer module

---
+++ kafka-consumer-database/src/main/java/com/utk/springbootkafka/SpringbootConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.utk.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConsumerApplication.class);
+    }
+}
